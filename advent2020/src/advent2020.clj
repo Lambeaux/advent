17a8b79d2901c3c9c831db7571cc7bc5067f45ee
Steven Lombardi <steve.lombardi@connexta.com>
2020-12-04 13:49:07 -0700

    Exercise one part one that doesn't really work for part two

---
+++ advent2020/src/advent2020.clj
@@ -0,0 +1,31 @@
+(ns advent2020)
+
+(defn- get-working-dir [] (System/getProperty "data.dir"))
+
+(defn- load-input-1 []
+  (map #(Integer/parseInt %)
+    (with-open [rdr (clojure.java.io/reader
+                      (str (get-working-dir) "/input1.txt"))]
+      (doall (line-seq rdr)))))
+
+(comment (println (slurp (str (get-working-dir) "/input1.txt"))) (get-working-dir))
+(comment (slurp) (str (get-working-dir) "/input1.txt"))
+
+(comment (frequencies (load-input-1)))
+
+(defn- solve-1 [inputs]
+  (let [all (map #(hash-map :orig % :diff (- 2020 %)) inputs)
+        agg (into #{} (map #(- 2020 %) inputs))]
+    (as-> all a
+         (filter #(contains? agg (:orig %)) a)
+         (first a)
+         (* (:orig a) (:diff a)))))
+
+(comment
+  (let [inputs (load-input-1)
+        all (map #(hash-map :orig % :diff (- 2020 %)) inputs)
+        agg (into #{} (map #(- 2020 %) inputs))]
+    agg))
+
+(comment (solve-1 (load-input-1)))
+(comment (map #(+ (:orig %) (:diff %)) (solve-1 (load-input-1))))
